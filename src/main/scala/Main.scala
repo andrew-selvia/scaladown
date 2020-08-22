de5253a94476a087edf5c2a808fcc04f64410a2b
Andrew Selvia <andrew@selvia.com>
2020-08-21 20:40:48 -0700

    Change of heart
    Instead of prefixing all internal links with "journal/", I've decided to move the files in GCS to a "journal" directory rather than being at the root. Now, the index.html at the root will make more sense.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -28,23 +28,22 @@ object Main extends IOApp {
               fmt.indentedChildren(List(SpanLink(content, ExternalTarget(s"#${opt.id.getOrElse("")}"))))
                 + fmt.newLine
             )
-//          case SpanLink(content, target, title, opt)  => fmt.element("a", opt, content, linkAttributes(target, title):_*)
-          case (fmt, SpanLink(content, target, title, opt)) =>
-            def linkAttributes (target: Target, title: Option[String]): Seq[(String, String)] = {
-              val href = target match {
-                case InternalTarget(absolutePath, relPath) =>
-                  // TODO - 0.16 - generalize suffix check
-//                  if (relPath.suffix.contains("md") && !relPath.toString.startsWith("..")) s"journal/${fmt.internalLink(relPath)}"
-//                  else relPath.toString
-                  s"journal${absolutePath.toString}"
-                case ExternalTarget(url) => url
-              }
-              fmt.optAttributes(
-                "href" -> Some(href),
-                "title" -> title.map(fmt.text)
-              )
-            }
-            fmt.element("a", opt, content, linkAttributes(target, title):_*)
+//          case (fmt, SpanLink(content, target, title, opt)) =>
+//            def linkAttributes (target: Target, title: Option[String]): Seq[(String, String)] = {
+//              val href = target match {
+//                case InternalTarget(absolutePath, relPath) =>
+//                  // TODO - 0.16 - generalize suffix check
+////                  if (relPath.suffix.contains("md") && !relPath.toString.startsWith("..")) s"journal/${fmt.internalLink(relPath)}"
+////                  else relPath.toString
+//                  s"journal${absolutePath.toString}"
+//                case ExternalTarget(url) => url
+//              }
+//              fmt.optAttributes(
+//                "href" -> Some(href),
+//                "title" -> title.map(fmt.text)
+//              )
+//            }
+//            fmt.element("a", opt, content, linkAttributes(target, title):_*)
           case (fmt, nl: NavigationList) =>
             def navigationToBulletList (navList: NavigationList): BulletList = {
               val bullet = StringBullet("*")
@@ -52,7 +51,13 @@ object Main extends IOApp {
                 items.map { item =>
                   val target: Block = item match {
                     case nh: NavigationHeader =>
-                      SpanSequence(nh.title.content, Styles("caret"))
+//                      SpanSequence(nh.title.content, Styles("caret"))
+                      SpanSequence(
+                        nh.title.content.head match {
+                          case text: Text if text.content == "/" => Seq(Text("Journal"))
+                          case _ => nh.title.content
+                        },
+                        Styles("caret"))
                     case nl: NavigationLink   =>
                       SpanSequence(Seq(SpanLink(nl.title.content, nl.target)), if (nl.content.isEmpty) NoOpt else Styles("caret"))
                   }
